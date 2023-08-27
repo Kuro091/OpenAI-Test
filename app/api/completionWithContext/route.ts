9324362abc6826a80e67777ad84e51417c89042b
Kuro091 <lcminhhp@gmail.com>
2023-08-27 16:48:39 +0700

    some tuning

--- app/api/completionWithContext/route.ts
+++ app/api/completionWithContext/route.ts
@@ -9,13 +9,24 @@ export const runtime = 'edge';
  
 export async function POST(req: Request) {
   const { prompt, context } = await req.json();
+
+// convert context, which is an object, to string
+  const contextString = JSON.stringify(context);
+  // remove all linebreaks
+
+  console.log('contextString', contextString.replaceAll("\n", ""))
+
   // Ask OpenAI for a streaming completion given the prompt
   const response = await openai.completions.create({
     model: 'text-davinci-003',
     stream: true,
-    temperature: 0.6,
+    temperature: 0.2,
+    n: 1,
+    stop: ['END OF THE QUESTION.'],
     max_tokens: 300,
-    prompt: `With the context of our conversation being ${context}, please answer this in a tldr kind of way: ${prompt}`,
+    prompt: `With the context of our conversation being ${contextString.replaceAll("\n", "")}, please answer this in a tldr kind of way: ${prompt}.
+    Don't try to finish my previous sentence. Just answer the question.
+    `,
   });
 
 
